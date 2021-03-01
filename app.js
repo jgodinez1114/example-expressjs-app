e01cbbbd4302f75feca95951b4a2dc35ebfda34c
jgodinez <jgodinez1114@gmail.com>
2021-03-01 16:38:31 -0600

    add sample value to logging text

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// start Joel Godinez
+// author: AgileNix
 
 // function to accept a str
 // add a parameter delay
@@ -22,4 +22,5 @@ const sleepNow = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-logMe('The Best Way To Get Started Is To Quit Talking And Begin Doing.', 1000, true)
+logMe('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 333, true);
+
