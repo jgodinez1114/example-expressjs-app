aa25fc656520815df6f0ea312fbbf755f2c4cc2e
jgodinez <jgodinez1114@gmail.com>
2021-03-01 16:20:43 -0600

    Add random output delay

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // start Joel Godinez
 
 // function to accept a str
-const logMe = (s) => {
-    for (const c of s) {
+// add a parameter delay
+// make it async
+const logMe = async (str, delay, randomized) => {
+    for (const c of str) {
+        // output the string char by char
         process.stdout.write(c);
+        // check for randomized delay
+        if (randomized) {
+            await sleepNow(Math.random * delay);
+        } else {
+            await sleepNow(delay);
+        };
     }
     process.stdout.write('\n');
 }
 
-logMe('this is a superb exe')
+// functon to sleep a necessary amount of time
+const sleepNow = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+logMe('The Best Way To Get Started Is To Quit Talking And Begin Doing.', 1000, true)
